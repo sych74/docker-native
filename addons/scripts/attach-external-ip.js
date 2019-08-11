1a1700ee64f2da283844045ab0faf5f18b2e43b9
sych74 <sych74@gmail.com>
2019-08-12 00:12:00 +0200

    Update attach-external-ip.js

--- addons/scripts/attach-external-ip.js
+++ addons/scripts/attach-external-ip.js
@@ -4,9 +4,8 @@ var resp = jelastic.billing.account.GetQuotas(appid, session, [
    'environment.externalip.maxcount.per.node'
 ].join(";"));
 
-if (ipQuotas.result != 0) return resp;
-ipQuotas = resp;
-resp = {result: 0};
+if (resp.result != 0) return resp;
+var ipQuotas = resp, resp = {result: 0};
 if (ipQuotas.array[0].value != 0 && ipQuotas.array[1].value > 0 && ipQuotas.array[2].value > 0) {   
    var r = jelastic.env.control.GetEnvInfo('${env.name}', session), nodes = [];
    if (r.result != 0) return r;
